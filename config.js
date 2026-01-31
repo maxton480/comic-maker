1f0b4e4c817b829ece6f204a14fe744de6e02db4
maxton480 <maxton480@gmail.com>
2026-02-01 01:04:06 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Main configuration file
-const config = {
-  fireworks: {
-    apiKey: process.env.FIREWORKS_API_KEY || '',
-    baseUrl: 'https://api.fireworks.ai/inference/v1',
-  }
-};
-
-module.exports = config;
